628241bf0b9bfd2ee87ae47b1d85e6beda21c0bc
mashkay <mashkay@gmail.com>
2024-09-30 19:24:27 +0200

    роутер через createBrowserRouter

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,13 +1,12 @@
-import { ConstructorPage } from '@pages';
 import '../../index.css';
 import styles from './app.module.css';
-
 import { AppHeader } from '@components';
+import { Outlet } from 'react-router-dom';
 
 const App = () => (
   <div className={styles.app}>
     <AppHeader />
-    <ConstructorPage />
+    <Outlet />
   </div>
 );
 
--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,61 @@
 import React from 'react';
 import * as ReactDOMClient from 'react-dom/client';
 import App from './components/app/app';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import {
+  ConstructorPage,
+  Feed,
+  ForgotPassword,
+  Login,
+  NotFound404,
+  Profile,
+  ProfileOrders,
+  Register,
+  ResetPassword
+} from '@pages';
 
 const container = document.getElementById('root') as HTMLElement;
 const root = ReactDOMClient.createRoot(container!);
 
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    children: [
+      {
+        path: '/',
+        element: <ConstructorPage />,
+        errorElement: <NotFound404 />
+      },
+      {
+        path: '/feed',
+        element: <Feed />
+      },
+      {
+        path: '/login',
+        element: <Login />
+      },
+      { path: '/register', element: <Register /> },
+      {
+        path: '/forgot-password',
+        element: <ForgotPassword />
+      },
+      { path: '/reset-password', element: <ResetPassword /> },
+      {
+        path: '/profile',
+        element: <Profile />
+      },
+      {
+        path: '/profile/orders',
+        element: <ProfileOrders />
+      },
+      { path: '*', element: <NotFound404 /> }
+    ]
+  }
+]);
+
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
