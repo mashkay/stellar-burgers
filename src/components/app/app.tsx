85fb714c0029322352c2279f1fbc09bd7e455546
mashkay <mashkay@gmail.com>
2024-10-01 06:01:30 +0200

    navLink

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,13 +1,42 @@
+import { ConstructorPage, Feed } from '@pages';
 import '../../index.css';
 import styles from './app.module.css';
-import { AppHeader } from '@components';
-import { Outlet } from 'react-router-dom';
+import { AppHeader, Modal } from '@components';
+import {
+  Outlet,
+  Route,
+  Routes,
+  useLocation,
+  useNavigate
+} from 'react-router-dom';
 
-const App = () => (
-  <div className={styles.app}>
-    <AppHeader />
-    <Outlet />
-  </div>
-);
+const App = () => {
+  const backgroundLocation = useLocation();
+  const navigate = useNavigate();
+  return (
+    <div className={styles.app}>
+      <AppHeader />
+      <Outlet />
+      {backgroundLocation && (
+        <Routes>
+          <Route
+            path='/'
+            element={
+              <Modal
+                title={'1'}
+                onClose={() => {
+                  navigate(-1);
+                  console.log('qwe');
+                }}
+              >
+                <ConstructorPage />
+              </Modal>
+            }
+          />
+        </Routes>
+      )}
+    </div>
+  );
+};
 
 export default App;
